19a80eac194962957be01099a0910c780eea087b
if <55980773+Yifeeeeei@users.noreply.github.com>
2023-11-27 12:31:07 -0800

    Update Databus.hs

--- hascii-player/app/Databus.hs
+++ hascii-player/app/Databus.hs
@@ -5,7 +5,7 @@ data Databus = Databus
     global_cache_path :: String,
     global_current_frame :: Int,
     global_total_frames :: Int,
-    ui2playbacklogic_status :: String,
-    playbacklogic2asciiloading_asciiart :: [[Char]],
-    asciiloading2playbacklogic_asciiart :: [[Char]]
+    gloab_asciiart :: [[Char]],
+
+    ui2playbacklogic_status :: String
   }
