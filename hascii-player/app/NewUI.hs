c62328705bcf1e0c0564dcc14294c0851a3efcee
if <55980773+Yifeeeeei@users.noreply.github.com>
2023-11-28 08:25:51 -0800

    constant changing ui

--- hascii-player/app/NewUI.hs
+++ hascii-player/app/NewUI.hs
@@ -9,6 +9,10 @@ import Brick.Widgets.Border.Style
 import Graphics.Vty.Input.Events
 import Graphics.Vty.Attributes
 import Databus
+import Control.Concurrent (forkIO, threadDelay)
+import Brick.BChan
+import Control.Monad (forever, void)
+import Graphics.Vty
 
 -- Video Window
 videoWindow :: Databus -> Widget ()
@@ -33,15 +37,24 @@ drawUI db = [uiLayout db]
                             , playPauseButton databus
                             , myProgressBar databus
                             ]
-appEvent :: Databus -> BrickEvent n e -> EventM n (Next Databus)
-appEvent db (VtyEvent (EvKey (KChar ' ') [])) = do
+-- appEvent :: Databus -> BrickEvent n e -> EventM n (Next Databus)
+-- appEvent db (VtyEvent (EvKey (KChar ' ') [])) = do
+--     let newStatus = if ui2playbacklogic_status db == "play" then "pause" else "play"
+--     let updatedDb = db { ui2playbacklogic_status = newStatus }
+--     continue $ get_asciiart updatedDb  -- Update ASCII art based on new status
+-- appEvent db (VtyEvent (EvKey KEsc [])) = halt db
+-- appEvent db _ = continue db
+
+appEvent :: Databus -> BrickEvent n Tick -> EventM n (Next Databus)
+appEvent db (AppEvent Tick) = continue $ get_asciiart db
+appEvent db (VtyEvent (EvKey (KChar ' ') [])) = 
     let newStatus = if ui2playbacklogic_status db == "play" then "pause" else "play"
-    let updatedDb = db { ui2playbacklogic_status = newStatus }
-    continue $ get_asciiart updatedDb  -- Update ASCII art based on new status
+    in continue $ get_asciiart db { ui2playbacklogic_status = newStatus }
 appEvent db (VtyEvent (EvKey KEsc [])) = halt db
 appEvent db _ = continue db
 
-app :: App Databus e ()
+
+app :: App Databus Tick ()
 app = App { appDraw = drawUI
           , appHandleEvent = appEvent
           , appStartEvent = return
@@ -49,12 +62,17 @@ app = App { appDraw = drawUI
           , appChooseCursor = neverShowCursor
           }
 
+-- get_asciiart :: Databus -> Databus
+-- get_asciiart db =
+--   case ui2playbacklogic_status db of
+--     "play" -> db { global_asciiart = ["Playing", "..."] }  -- Replace with actual ASCII art for playing
+--     "pause" -> db { global_asciiart = ["Pausing", "..."] } -- Replace with actual ASCII art for pausing
+--     _ -> db  -- Default case, can be modified as needed
 get_asciiart :: Databus -> Databus
 get_asciiart db =
-  case ui2playbacklogic_status db of
-    "play" -> db { global_asciiart = ["Playing", "..."] }  -- Replace with actual ASCII art for playing
-    "pause" -> db { global_asciiart = ["Pausing", "..."] } -- Replace with actual ASCII art for pausing
-    _ -> db  -- Default case, can be modified as needed
+    let nextFrame = (global_current_frame db + 1) `mod` global_total_frames db
+    in db { global_current_frame = nextFrame, global_asciiart = ["Frame: " ++ show nextFrame] }
+data Tick = Tick
 
 
 get_databus :: Databus -> IO Databus
@@ -73,8 +91,18 @@ get_databus db = do
 --         loop db'
 --    loop initialDatabus
 
+-- new_ui_main :: IO ()
+-- new_ui_main = do
+--    let initialDatabus = MakeDatabus "" "" 0 100 ["Initial", "State"] "play"
+--    defaultMain app $ get_asciiart initialDatabus
+--    return ()
 new_ui_main :: IO ()
 new_ui_main = do
-   let initialDatabus = MakeDatabus "" "" 0 100 ["Initial", "State"] "play"
-   defaultMain app $ get_asciiart initialDatabus
-   return ()
+    let initialDatabus = MakeDatabus "" "" 0 100 [] "play"
+    chan <- newBChan 10
+    _ <- forkIO $ forever $ do
+        writeBChan chan Tick
+        threadDelay 1000000  -- 1 second
+    let buildVty = mkVty defaultConfig
+    initialVty <- buildVty
+    void $ customMain initialVty buildVty (Just chan) app initialDatabus
