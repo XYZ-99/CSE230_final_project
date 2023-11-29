5bf5fc915f02ca752e7aea871f51afeb4b6cb983
neo1124 <yuning@ucsd.edu>
2023-11-28 23:19:36 -0800

    Third version of PlaybackLogic

--- hascii-player/app/PlaybackLogic.hs
+++ hascii-player/app/PlaybackLogic.hs
@@ -1,43 +1,62 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 module PlaybackLogic where
 
 import Databus
 import ASCIILoading
 import qualified Data.Text as T
 
---  handle I/O operations and calling playbacklogic_main
-updateDatabusWithImage :: Databus -> IO Databus
-updateDatabusWithImage databus = do
-    let frame_path = global_cache_path databus ++ "/" ++ show (global_current_frame databus) ++ ".png"
+--  main
+playbacklogic_main :: Databus -> IO Databus
+playbacklogic_main databus = do
+    let framePath = constructFramePath databus
     let targetWidth = 800
-    maybeImage <- loadImage frame_path targetWidth
+    maybeImage <- loadImage framePath targetWidth
     case maybeImage of
         Just image -> do
             let asciiArt = lines (T.unpack (toAsciiArt image))
-            return $ playbacklogic_main databus asciiArt
-        Nothing -> return databus
+            return $ updateDatabus databus asciiArt
+        Nothing -> do
+            putStrLn "Error: Failed to load image."
+            return databus
 
--- accepts ASCII art as a parameter and returns Databus
-playbacklogic_main :: Databus -> [String] -> Databus
-playbacklogic_main databus asciiArt =
-    let new_current_frame_index = playbacklogic_calc_current_frame databus
-    in MakeDatabus {
-        global_video_path       = global_video_path databus,
-        global_cache_path       = global_cache_path databus,
-        global_current_frame    = new_current_frame_index,
-        global_total_frames     = global_total_frames databus,
-        global_asciiart         = asciiArt,
-        ui2playbacklogic_status = ui2playbacklogic_status databus
-    }
+-- Construct frame path
+constructFramePath :: Databus -> String
+constructFramePath databus = global_cache_path databus ++ "/" ++ show (global_current_frame databus) ++ ".png"
+
+-- Construct Databus
+constructDatabus :: Databus -> [String] -> Int -> Databus
+constructDatabus databus asciiArt newCurrentFrameIndex = MakeDatabus {
+    global_video_path       = global_video_path databus,
+    global_cache_path       = global_cache_path databus,
+    global_current_frame    = newCurrentFrameIndex,
+    global_total_frames     = global_total_frames databus,
+    global_asciiart         = asciiArt,
+    ui2playbacklogic_status = ui2playbacklogic_status databus
+}
+
+-- Update and return Databus
+updateDatabus :: Databus -> [String] -> Databus
+updateDatabus databus asciiArt =
+    let newCurrentFrameIndex = playbacklogic_calc_current_frame databus
+    in constructDatabus databus asciiArt newCurrentFrameIndex
 
 playbacklogic_calc_current_frame :: Databus -> Int
 playbacklogic_calc_current_frame databus
-  | ui2playbacklogic_status databus == "play"  = nextFrameIndex databus
+  | status == "play"  = nextFrameIndex 
+  | status == "fast-forward" = fastForwardIndex 
+  | status == "rewind" = rewindIndex 
   | otherwise = global_current_frame databus
   where
-    nextFrameIndex db
+    status = ui2playbacklogic_status databus
+    nextFrameIndex 
       | currentFrame < totalFrames - 1 = currentFrame + 1
       | otherwise = totalFrames - 1
-      -- otherwise = 0
-      where
-        currentFrame = global_current_frame db
-        totalFrames = global_total_frames db
+    fastForwardIndex 
+      | currentFrame + 10 < totalFrames = currentFrame + 10
+      | otherwise = totalFrames - 1
+    rewindIndex 
+      | currentFrame - 10 >= 0 = currentFrame - 10
+      | otherwise = 0
+    currentFrame = global_current_frame databus
+    totalFrames = global_total_frames databus
